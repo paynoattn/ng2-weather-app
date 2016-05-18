d9a911ebf4e78ec1c0300d211be22b8dd7fc2f83
Christopher Pawlukiewicz <hi@paynoattn.com>
2016-05-17 23:05:00 -0600

    added placeholder routes

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+let aboutTemplate = require('./about.html');
+
+@Component({
+    selector: 'about',
+    template: aboutTemplate
+})
+
+export class About { }
---
+++ src/app/example/example.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+let exTemplate = require('./example.html');
+
+@Component({
+    selector: 'example',
+    template: exTemplate
+})
+
+export class Example { }
